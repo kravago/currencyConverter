9623e55f1730837ab0abc0bd151d57e7bbd3c0b0
Engines-screaming <karl.ravago@gmail.com>
2021-12-07 18:09:58 -0800

    able to use decimals in amount field

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 from forex_python.converter import CurrencyRates
+from decimal import *
 
-c = CurrencyRates()
+c = CurrencyRates(force_decimal=True)
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,7 +14,7 @@ def convert():
     currency1 = request.args.get('currency1')
     currency2 = request.args.get('currency2')
     amount = request.args.get('amount')
-    result = c.convert(base_cur=currency1, dest_cur=currency2, amount=int(amount))
+    result = c.convert(base_cur=currency1, dest_cur=currency2, amount=Decimal(amount))
 
     return render_template('results.html',
                             amount=amount,
