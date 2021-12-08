ae60c8da82238b366ca8e92f1f0681f539658474
Engines-screaming <karl.ravago@gmail.com>
2021-12-07 19:00:31 -0800

    shows symbol on results page and added button to return

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
-from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyRates, CurrencyCodes
 from decimal import *
 
 c = CurrencyRates(force_decimal=True)
+codes = CurrencyCodes()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,8 +19,6 @@ def convert():
     result = c.convert(base_cur=currency1, dest_cur=currency2, amount=Decimal(amount))
 
     return render_template('results.html',
-                            amount=amount,
-                            currency1=currency1,
-                            currency2=currency2,
+                            symbol=codes.get_symbol(currency2),
                             result=round(result, 2)
                             )
